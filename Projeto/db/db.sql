26f7a35d1d69748f22b289284982174004f68a45
Maria Clara <123563801+maria-kaki@users.noreply.github.com>
2023-09-15 15:09:03 -0300

    remove
    remove

--- Projeto/db/db.sql
+++ Projeto/db/db.sql
@@ -23,19 +23,3 @@ CREATE TABLE working_hours (
     FOREIGN KEY (user_id) REFERENCES users(id),
     CONSTRAINT cons_user_day UNIQUE (user_id, work_date)
 );
-
--- Essa senha criptografada corresponde ao valor "a"
-INSERT INTO users (id, name, password, email, start_date, end_date, is_admin)
-VALUES (1, 'Admin', '$2y$10$/vC1UKrEJQUZLN2iM3U9re/4DQP74sXCOVXlYXe/j9zuv1/MHD4o.', 'admin@cod3r.com.br', '2000-1-1', null, 1);
-
-INSERT INTO users (id, name, password, email, start_date, end_date, is_admin)
-VALUES (2, 'Chaves', '$2y$10$/vC1UKrEJQUZLN2iM3U9re/4DQP74sXCOVXlYXe/j9zuv1/MHD4o.', 'chaves@cod3r.com.br', '2000-1-1', null, 1);
-
-INSERT INTO users (id, name, password, email, start_date, end_date, is_admin)
-VALUES (3, 'Seu Barriga', '$2y$10$/vC1UKrEJQUZLN2iM3U9re/4DQP74sXCOVXlYXe/j9zuv1/MHD4o.', 'barriga@cod3r.com.br', '2000-1-1', null, 0);
-
-INSERT INTO users (id, name, password, email, start_date, end_date, is_admin)
-VALUES (4, 'Seu Madruga', '$2y$10$/vC1UKrEJQUZLN2iM3U9re/4DQP74sXCOVXlYXe/j9zuv1/MHD4o.', 'madruga@cod3r.com.br', '2000-1-1', null, 0);
-
-INSERT INTO users (id, name, password, email, start_date, end_date, is_admin)
-VALUES (5, 'Quico', '$2y$10$/vC1UKrEJQUZLN2iM3U9re/4DQP74sXCOVXlYXe/j9zuv1/MHD4o.', 'quico@cod3r.com.br', '2000-1-1', '2019-1-1', 0);
